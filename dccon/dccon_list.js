1c0153f29056392e1677a98e58f9c33233d77bda
gocho2 <56300436+gocho2@users.noreply.github.com>
2019-12-29 17:50:12 +0900

    Update dccon_list.js

--- dccon/dccon_list.js
+++ dccon/dccon_list.js
@@ -1,7 +1,121 @@
 
 dcConsData = [
+ 
+  	///김정은//
+	{name:"우리핵박수.gif",		keywords:["우리핵박수"],	tags:["북한", "김정은"]},
+	{name:"우리핵박수2.gif",	keywords:["우리핵박수2"],	tags:["북한", "김정은"]},
+	{name:"우리핵박수3.gif",	keywords:["우리핵박수3"],	tags:["북한", "김정은"]},
+	{name:"우리핵빵야.gif",		keywords:["우리핵빵야"],	tags:["북한", "김정은"]},
+	{name:"우리핵쌍안경.gif",	keywords:["우리핵쌍안경"],	tags:["북한", "김정은"]},
+	{name:"우리핵아닌데.gif",	keywords:["우리핵아닌데"],	tags:["북한", "김정은"]},
+	{name:"우리핵안녕.gif",	    keywords:["우리핵안녕"],	tags:["북한", "김정은"]},
+	{name:"우리핵투정.gif",   	keywords:["우리핵투정"],	tags:["북한", "김정은"]},
+
 	
-	{"name":"우리핵박수.gif",           "keywords":["우리핵박수"],                      "tags":["북한"]}
+	
+    //ppap//
+   	{name:"PPAP.gif",		keywords:["PPAP"],	tags:["PPAP"]},
+	
+	
+	//군대//
+	{name:"굳건이.jpg",		keywords:["굳건이"],	tags:["군대"]},
+	
+	
+	
+	//오버워치//
+	{name:"눈물한조.png",		keywords:["눈물한조"],	tags:["오버워치"]},
+	
+	
+	//해외정치인//
+	{name:"럼프따봉.png",		keywords:["럼프따봉"],	tags:["트럼프"]},
+	{name:"리듬스탈.gif",		keywords:["리듬스탈"],	tags:["스탈린"]},
 
+	
+	//WWE//
+	{name:"맥절정.gif",		keywords:["맥절정"],	tags:["wwe"]},
+	
+	
+	
+	//김두한//
+	{name:"오케이땡큐.gif",		keywords:["오케이땡큐"],	tags:["김두한","야인시대"]},
+    {name:"4딸라,png",		keywords:["4딸라"],	tags:["김두한","야인시대"]},
+	
+	
+	
+	
+	////////////롤코타/////////////
+	{name:"멀미.gif",		    keywords:["멀미"],	tags:["롤코타"]},
+	{name:"멀미2.gif",		keywords:["멀미2"],	tags:["롤코타"]},
+	{name:"멀미3.gif",		keywords:["멀미3"],	tags:["롤코타"]},
+	{name:"피곤.gif",		    keywords:["피곤"],	tags:["롤코타"]},
+	{name:"행복.gif",		    keywords:["행복"],	tags:["롤코타"]},
+	{name:"행복2.gif",	    keywords:["행복2"],	tags:["롤코타"]},
+	{name:"화남.gif",	        keywords:["화남"],	tags:["롤코타"]},
+	{name:"ATM.png",		keywords:["atm"],	tags:["롤코타"]},
+	{name:"화장실.png",		keywords:["화장실"],	tags:["롤코타"]},
+	{name:"회전목마.png",		keywords:["회전목마"],	tags:["롤코타"]},
+	{name:"불행.gif",	     	keywords:["불행"],	tags:["롤코타"]},
+	{name:"불행2.gif",		keywords:["불행2"],	tags:["롤코타"]},
+    {name:"안내소.png",		keywords:["안내소"],	tags:["롤코타"]},	
+	//호랑이/////////
+	
+	{name:"호랑이.gif",	    keywords:["호랑이"],	tags:["호랑이"]},
+	{name:"호랑이2.gif",	    keywords:["호랑이2"],	tags:["호랑이"]},
+		
+	
+	
+	
+	////당당콘////
+	
+	{name:"문풍당당.gif",		keywords:["문풍당당"],	tags:["당당"]},
+	{name:"문풍당당2.gif",		keywords:["문풍당당2"],	tags:["당당"]},
+	{name:"이풍당당.gif",		keywords:["이풍당당"],	tags:["당당"]},
+	{name:"이풍당당2.gif",		keywords:["이풍당당2"],	tags:["당당"]},
 
-];
+	//볼트보이//
+	{name:"볼트게이.png",		keywords:["볼트게이"],	tags:["볼트보이"]},
+	{name:"볼트딸딸.png",		keywords:["볼트딸딸"],	tags:["볼트보이"]},
+	{name:"볼트천사.png",		keywords:["볼트천사"],	tags:["볼트보이"]},
+	
+	
+	////편안콘//
+	{name:"불편.png",		    keywords:["불편"],	tags:["불편"]},
+	{name:"편안.png",		    keywords:["편안"],	tags:["편안"]},
+	
+	
+	//퓨디파이//
+	{name:"어케이겼노.gif",		keywords:["어케이겼노"],	tags:["퓨디파이"]},
+	{name:"어케했노.gif",		keywords:["어케했노"],	tags:["퓨디파이"]},
+	
+	
+	
+	//게이//
+	{name:"아아.png",		    keywords:["아아"],	tags:["게이"]},
+	{name:"마치.png",		    keywords:["마치"],	tags:["게이"]},
+	
+	
+
+	
+	//국뽕//
+    {name:"펄럭.gif",		keywords:["펄럭"],	tags:["국뽕"]},
+	{name:"펄럭2.gif",    keywords:["펄럭2"],	tags:["국뽕"]},
+	
+	
+	
+	
+	//페페//
+	{name:"페페발암.png",    keywords:["페페발암"],	tags:["페페"]},
+	{name:"페페흐음.png",    keywords:["페페흐음"],	tags:["페페"]},
+	
+	
+		//잡짤////
+	{name:"에디와우.gif",		keywords:["에디와우"],	tags:["와우"]},
+    {name:"주인님.png",		keywords:["주인님"],			tags:["자살"]},
+    {name:"겉바속촉.gif", 	    keywords:["겉바속촉"],	tags:["와우", "헬스크림"]},
+	{name:"흠.png", 	    keywords:["흠"],	tags:["흠"]}
+    
+	
+	
+
+	
+	];
